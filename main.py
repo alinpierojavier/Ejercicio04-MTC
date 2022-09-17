ad9939d788c7385c001b4f2783ee74e69a831fbb
Piero <32024150+alinpierojavier@users.noreply.github.com>
2022-09-17 16:33:10 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+#Recibir una palabra y número e imprimir número de veces dicha palabra.
+palabra=str(input("Ingresa una palabra:\n"))
+veces=int(input("Ingresa un numero de veces:\n"))
+i=0
+while i<=veces:
+ print(palabra)
+ i=i+1
